e3243380105718a5820d148a2a450c6edc8997d8
haddockdaniel <43544084+haddockdaniel@users.noreply.github.com>
2018-10-05 09:09:31 -0400

    Initial commit

---
+++ JurisUtilityBase/UtilityBaseMain.Designer.cs
@@ -0,0 +1,355 @@
+﻿namespace JurisUtilityBase
+{
+    partial class UtilityBaseMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UtilityBaseMain));
+            this.JurisLogoImageBox = new System.Windows.Forms.PictureBox();
+            this.LexisNexisLogoPictureBox = new System.Windows.Forms.PictureBox();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.listBoxCompanies = new System.Windows.Forms.ListBox();
+            this.labelDescription = new System.Windows.Forms.Label();
+            this.statusGroupBox = new System.Windows.Forms.GroupBox();
+            this.labelCurrentStatus = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.labelPercentComplete = new System.Windows.Forms.Label();
+            this.OpenFileDialogOpen = new System.Windows.Forms.OpenFileDialog();
+            this.buttonReport = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxRecDate = new System.Windows.Forms.TextBox();
+            this.textBoxBegBal = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxStateDate = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxEndBal = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.JurisLogoImageBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LexisNexisLogoPictureBox)).BeginInit();
+            this.statusStrip.SuspendLayout();
+            this.statusGroupBox.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // JurisLogoImageBox
+            // 
+            this.JurisLogoImageBox.Image = ((System.Drawing.Image)(resources.GetObject("JurisLogoImageBox.Image")));
+            this.JurisLogoImageBox.InitialImage = ((System.Drawing.Image)(resources.GetObject("JurisLogoImageBox.InitialImage")));
+            this.JurisLogoImageBox.Location = new System.Drawing.Point(0, 1);
+            this.JurisLogoImageBox.Name = "JurisLogoImageBox";
+            this.JurisLogoImageBox.Size = new System.Drawing.Size(104, 336);
+            this.JurisLogoImageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.JurisLogoImageBox.TabIndex = 0;
+            this.JurisLogoImageBox.TabStop = false;
+            // 
+            // LexisNexisLogoPictureBox
+            // 
+            this.LexisNexisLogoPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("LexisNexisLogoPictureBox.Image")));
+            this.LexisNexisLogoPictureBox.Location = new System.Drawing.Point(8, 343);
+            this.LexisNexisLogoPictureBox.Name = "LexisNexisLogoPictureBox";
+            this.LexisNexisLogoPictureBox.Size = new System.Drawing.Size(96, 28);
+            this.LexisNexisLogoPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.LexisNexisLogoPictureBox.TabIndex = 1;
+            this.LexisNexisLogoPictureBox.TabStop = false;
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel});
+            this.statusStrip.Location = new System.Drawing.Point(0, 374);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(658, 22);
+            this.statusStrip.TabIndex = 2;
+            // 
+            // toolStripStatusLabel
+            // 
+            this.toolStripStatusLabel.BackColor = System.Drawing.SystemColors.Control;
+            this.toolStripStatusLabel.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+            this.toolStripStatusLabel.Size = new System.Drawing.Size(134, 17);
+            this.toolStripStatusLabel.Text = "Status: Ready to Execute";
+            // 
+            // listBoxCompanies
+            // 
+            this.listBoxCompanies.FormattingEnabled = true;
+            this.listBoxCompanies.Location = new System.Drawing.Point(111, 1);
+            this.listBoxCompanies.Name = "listBoxCompanies";
+            this.listBoxCompanies.Size = new System.Drawing.Size(262, 69);
+            this.listBoxCompanies.TabIndex = 0;
+            this.listBoxCompanies.SelectedIndexChanged += new System.EventHandler(this.listBoxCompanies_SelectedIndexChanged);
+            // 
+            // labelDescription
+            // 
+            this.labelDescription.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelDescription.Location = new System.Drawing.Point(380, 1);
+            this.labelDescription.Name = "labelDescription";
+            this.labelDescription.Size = new System.Drawing.Size(268, 69);
+            this.labelDescription.TabIndex = 1;
+            this.labelDescription.Text = "This tool will undo the last recorded bank reconciliation, restoring it to the po" +
+    "int immediately before it was recorded. All cleared items remain cleared.";
+            // 
+            // statusGroupBox
+            // 
+            this.statusGroupBox.Controls.Add(this.labelCurrentStatus);
+            this.statusGroupBox.Controls.Add(this.progressBar);
+            this.statusGroupBox.Controls.Add(this.labelPercentComplete);
+            this.statusGroupBox.Location = new System.Drawing.Point(111, 77);
+            this.statusGroupBox.Name = "statusGroupBox";
+            this.statusGroupBox.Size = new System.Drawing.Size(537, 73);
+            this.statusGroupBox.TabIndex = 5;
+            this.statusGroupBox.TabStop = false;
+            this.statusGroupBox.Text = "Utility Status:";
+            // 
+            // labelCurrentStatus
+            // 
+            this.labelCurrentStatus.AutoSize = true;
+            this.labelCurrentStatus.Location = new System.Drawing.Point(7, 50);
+            this.labelCurrentStatus.Name = "labelCurrentStatus";
+            this.labelCurrentStatus.Size = new System.Drawing.Size(77, 13);
+            this.labelCurrentStatus.TabIndex = 2;
+            this.labelCurrentStatus.Text = "Current Status:";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(7, 27);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(519, 20);
+            this.progressBar.TabIndex = 0;
+            // 
+            // labelPercentComplete
+            // 
+            this.labelPercentComplete.AutoSize = true;
+            this.labelPercentComplete.Location = new System.Drawing.Point(413, 11);
+            this.labelPercentComplete.Name = "labelPercentComplete";
+            this.labelPercentComplete.Size = new System.Drawing.Size(62, 13);
+            this.labelPercentComplete.TabIndex = 0;
+            this.labelPercentComplete.Text = "% Complete";
+            // 
+            // OpenFileDialogOpen
+            // 
+            this.OpenFileDialogOpen.FileName = "openFileDialog1";
+            // 
+            // buttonReport
+            // 
+            this.buttonReport.BackColor = System.Drawing.Color.LightGray;
+            this.buttonReport.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonReport.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.buttonReport.Location = new System.Drawing.Point(133, 321);
+            this.buttonReport.Name = "buttonReport";
+            this.buttonReport.Size = new System.Drawing.Size(148, 38);
+            this.buttonReport.TabIndex = 16;
+            this.buttonReport.Text = "Exit";
+            this.buttonReport.UseVisualStyleBackColor = false;
+            this.buttonReport.Click += new System.EventHandler(this.buttonReport_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightGray;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.button1.Location = new System.Drawing.Point(489, 321);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 38);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(118, 174);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Bank Account";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(214, 171);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(398, 21);
+            this.comboBox1.TabIndex = 19;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBoxEndBal);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.textBoxStateDate);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBoxBegBal);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBoxRecDate);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(121, 201);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(491, 100);
+            this.groupBox1.TabIndex = 20;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Most Recent Recorded Bank Reconciliation";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Recorded Date";
+            // 
+            // textBoxRecDate
+            // 
+            this.textBoxRecDate.Location = new System.Drawing.Point(108, 28);
+            this.textBoxRecDate.Name = "textBoxRecDate";
+            this.textBoxRecDate.Size = new System.Drawing.Size(132, 20);
+            this.textBoxRecDate.TabIndex = 1;
+            // 
+            // textBoxBegBal
+            // 
+            this.textBoxBegBal.Location = new System.Drawing.Point(108, 60);
+            this.textBoxBegBal.Name = "textBoxBegBal";
+            this.textBoxBegBal.Size = new System.Drawing.Size(132, 20);
+            this.textBoxBegBal.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 63);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Beginning Balance";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBoxStateDate
+            // 
+            this.textBoxStateDate.Location = new System.Drawing.Point(353, 28);
+            this.textBoxStateDate.Name = "textBoxStateDate";
+            this.textBoxStateDate.Size = new System.Drawing.Size(132, 20);
+            this.textBoxStateDate.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(254, 31);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Statement Date";
+            // 
+            // textBoxEndBal
+            // 
+            this.textBoxEndBal.Location = new System.Drawing.Point(353, 63);
+            this.textBoxEndBal.Name = "textBoxEndBal";
+            this.textBoxEndBal.Size = new System.Drawing.Size(132, 20);
+            this.textBoxEndBal.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(254, 66);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Ending Balance";
+            // 
+            // UtilityBaseMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(658, 396);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonReport);
+            this.Controls.Add(this.statusGroupBox);
+            this.Controls.Add(this.labelDescription);
+            this.Controls.Add(this.listBoxCompanies);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.LexisNexisLogoPictureBox);
+            this.Controls.Add(this.JurisLogoImageBox);
+            this.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "UtilityBaseMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "JPS - Undo Bank Rec";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.JurisLogoImageBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LexisNexisLogoPictureBox)).EndInit();
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.statusGroupBox.ResumeLayout(false);
+            this.statusGroupBox.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox JurisLogoImageBox;
+        private System.Windows.Forms.PictureBox LexisNexisLogoPictureBox;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
+        private System.Windows.Forms.ListBox listBoxCompanies;
+        private System.Windows.Forms.Label labelDescription;
+        private System.Windows.Forms.GroupBox statusGroupBox;
+        private System.Windows.Forms.Label labelCurrentStatus;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label labelPercentComplete;
+        private System.Windows.Forms.OpenFileDialog OpenFileDialogOpen;
+        private System.Windows.Forms.Button buttonReport;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBoxEndBal;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxStateDate;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxBegBal;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxRecDate;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
